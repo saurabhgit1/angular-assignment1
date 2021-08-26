c2d8505fb3bc85914abfc5fd8fff910107eb6845
saurabhgit1 <singhsaurabhkumar84@gmail.com>
2021-08-26 22:39:28 +0530

    assignment angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngclassdemo';
+
+  arr=['first line', 'second line', 'third line', 'fourth line'];
+
+  // listofclasses1 ={
+  //   "special":true
+  // }
+}
